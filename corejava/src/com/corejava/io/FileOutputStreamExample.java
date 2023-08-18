73f2b7d6a9a266590440d49880396755dd67b0fb
Sunil P <sunil.p@valtech.com>
2023-08-18 18:09:43 +0530

    io example

---
+++ corejava/src/com/corejava/io/FileOutputStreamExample.java
@@ -0,0 +1,20 @@
+package com.corejava.io;
+
+import java.io.FileOutputStream;
+
+public class FileOutputStreamExample {
+
+	public static void main(String args[]) {
+		try {
+			FileOutputStream fout = new FileOutputStream("D:\\testout.txt");
+			String s = "My first program in I/O";
+			byte b[] = s.getBytes();// converting string into byte array
+			fout.write(b);
+			fout.close();
+			System.out.println("success...");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
