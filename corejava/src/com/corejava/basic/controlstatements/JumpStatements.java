3629184662f9ce63f7fac10e4cadcad07baff460
Sunil P <sunil.p@valtech.com>
2023-08-18 18:33:26 +0530

    input stream example

--- corejava/src/com/corejava/basic/controlstatements/JumpStatements.java
+++ corejava/src/com/corejava/basic/controlstatements/JumpStatements.java
@@ -26,18 +26,17 @@ public class JumpStatements {
 			System.out.print(i + " ");
 		}
 	}
-	
-	void testBreak(){
-		
+
+	void testBreak() {
+
 		// Initially loop is set to run from 0-9
-        for (int i = 0; i < 10; i++) {
-            // terminate loop when i is 5.
-            if (i == 5)
-                break;
- 
-            System.out.println("i: " + i);
-        }
-        System.out.println("Loop complete.");
-    }
+		for (int i = 0; i < 10; i++) {
+			// terminate loop when i is 5.
+			if (i == 5)
+				break;
+
+			System.out.println("i: " + i);
+		}
+		System.out.println("Loop complete.");
 	}
 }
