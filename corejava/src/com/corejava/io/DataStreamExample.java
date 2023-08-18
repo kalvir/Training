59a5b573b2454f8cc2f9daa710adbb7885eebf6d
Sunil P <sunil.p@valtech.com>
2023-08-18 20:17:28 +0530

    input

--- corejava/src/com/corejava/io/DataStreamExample.java
+++ corejava/src/com/corejava/io/DataStreamExample.java
@@ -5,12 +5,22 @@ import java.io.FileInputStream;
 public class DataStreamExample {
 	public static void main(String args[]) {
 		try {
+			// while loop
 			FileInputStream fin = new FileInputStream("D:\\testout.txt");
 			int i = 0;
-			while ((i = fin.read()) != -1) {
+			while ((i = fin.read()) != -1) { // System.out.println(i);
 				System.out.print((char) i);
 			}
+
 			fin.close();
+			// for loop
+			System.out.println();
+			FileInputStream fins = new FileInputStream("D:\\testout.txt");
+			for (; (i = fins.read()) != -1;) {
+
+				System.out.print((char) i);
+			}
+			fins.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
